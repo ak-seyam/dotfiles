fb577ca3a8dfe3f3bb9035bc1bc6d936f8904b6e
A-Siam <abdullah.khaled.kamal0@gmail.com>
2022-11-19 15:47:49 +0200

    remove unused roles

--- install.sh
+++ install.sh
@@ -3,10 +3,8 @@ read -p "Warning: this script assumes you are using Fedora (with 3rd party repos
 
 sudo dnf update
 sudo dnf install -y curl ansible
-sudo ansible-galaxy install gantsign.intellij
 sudo ansible-galaxy install gantsign.postman
 sudo ansible-galaxy install gantsign.visual-studio-code
-sudo ansible-galaxy install darkwizard242.googlechrome
 
 sudo ansible-pull -U https://github.com/A-Siam/dotfiles -C fedora --extra-vars="username=$USER"
 cat "/home/$USER/dconf.ini" | dconf load /
