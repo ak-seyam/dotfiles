2af62732fc49496852e3f86903c907fde3f52d86
A-Siam <abdullah.khaled.kamal0@gmail.com>
2022-11-21 13:50:39 +0200

    update install and local.yml

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 read -p "Warning: this script assumes you are using Fedora (with 3rd party repositories) based distro [Press return to continue]"
 
-sudo dnf update
+sudo dnf update -y
 sudo dnf install -y curl ansible
 sudo ansible-galaxy install gantsign.postman
 sudo ansible-galaxy install gantsign.visual-studio-code
