87e8ac020384deb8b605774ec75d40960866e562
A-Siam <abdullah.khaled.kamal0@gmail.com>
2022-12-10 22:31:08 +0200

    feat: update fish commands

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set fish_greeting
+# aliases
+alias ga="git add -A"
+alias gc="git commit"
+alias gp="git push"
+alias si="sudo dnf install"
+alias sup="sudo dnf update"
