96b6008793856444bd952a0d4471d6d708d9f689
ak-seyam <abdullah.khaled.kamal0@gmail.com>
2026-03-20 03:27:59 +0200

    Rethinking minimal config

--- .scripts/recording.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ffmpeg -f alsa -ac 2 -i default -f x11grab -i $DISPLAY "$(date +%s).mp4"
--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-set fish_greeting
-abbr --add si "sudo apt install"
-abbr --add srm "sudo apt remove --purge"
-abbr --add sup "sudo apt update && sudo apt upgrade"
-abbr --add sx "startx"
-abbr --add p "python"
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
