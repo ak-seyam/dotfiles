0bab7946f43324474b21826022ef13eb1839dd0e
ak-seyam <abdullah.khaled.kamal0@gmail.com>
2026-03-24 08:45:30 +0200

    Shell: add fish config

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+set fish_greeting
+set spanish_study_path "/mnt/27AE92097B9FEE4C/spanish_words"
+alias sx="startx"
+alias gss="cd $spanish_study_path"
+fish_vi_key_bindings
+set -g EDITOR "vim"
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
