edbc681c92eb691e1b64a7b35adaf786db6fb736
ak-seyam <abdullah.khaled.kamal0@gmail.com>
2026-04-12 11:01:01 +0200

    QoL: alias for updating the system

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ set fish_greeting
 set spanish_study_path "/mnt/27AE92097B9FEE4C/spanish_words"
 alias sx="startx"
 alias gss="cd $spanish_study_path"
+alias upd="sudo pacman -Syu"
 fish_vi_key_bindings
 set -g EDITOR "vim"
 if status is-interactive
